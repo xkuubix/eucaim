8ba5d80251aaa92c1174e7d9a017754e134a7331
xkuubix <buler.jakub@gmail.com>
2025-08-01 15:33:38 +0200

    updated paths

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ sys.path.append('./code')
 import pandas as pd
 from pydicom import dcmread
 
-DICOM_DIR = os.path.join('euc_gumed_u6_v2')
-CSV_FILE = os.path.join('EuCanImageUseCase68_1.csv')
+DICOM_DIR = os.path.join('../euc_gumed_u6_v2')
+CSV_FILE = os.path.join('../EuCanImageUseCase68_1.csv')
 SUBJECT_DIR = sorted(os.listdir(DICOM_DIR))
 
 
@@ -18,7 +18,7 @@ subset_data = data[data['patientclass'] == 0]
 # %%
 from file_manipulation import make_table
 if __name__ == "__main__":
-    make_table(DICOM_DIR)
+    df = make_table(DICOM_DIR)
 
 # %%
 from rts2nii import rtstruct2nii
