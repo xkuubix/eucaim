6d37578f8c7a24878e1976fbcc8670f2f7ac488d
xkuubix <buler.jakub@gmail.com>
2025-08-08 16:21:22 +0200

    merge full data

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ sys.path.append('./code')
 import pandas as pd
 from pydicom import dcmread
 
-DICOM_DIR = os.path.join('../euc_gumed_u6_v2')
-CSV_FILE = os.path.join('../EuCanImageUseCase68_1.csv')
-SUBJECT_DIR = sorted(os.listdir(DICOM_DIR))
+os.chdir('/users/project1/pt01190/EUCAIM-PG-GUM/code')
 
+# DICOM_DIR = os.path.join('../euc_gumed_u6_v2')
+DICOM_DIR = os.path.join('../UC6')
+# CSV_FILE = os.path.join('../EuCanImageUseCase68_1.csv')
+CSV_FILE = os.path.join('../EuCanImageUseCase68_v2.csv')
+SUBJECT_DIR = sorted([
+    d for d in os.listdir(DICOM_DIR)
+    if os.path.isdir(os.path.join(DICOM_DIR, d))
+])
 
 data = pd.read_csv(CSV_FILE,
-                   delimiter=';',
+                   delimiter=',',
                    encoding='utf-8')
 
 subset_data = data[data['patientclass'] == 0]
@@ -21,10 +27,11 @@ if __name__ == "__main__":
     df = make_table(DICOM_DIR)
 
 # %%
+merged_data = pd.merge(data, df, how='inner', on='record_id')
+# %%
 from rts2nii import rtstruct2nii
 
 if __name__ == "__main__":
     rtstruct2nii(DICOM_DIR)
-
 
 # %%
