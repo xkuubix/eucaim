e5933c219af74f6bac868cdb57cd2fd5ee36bafb
xkuubix <buler.jakub@gmail.com>
2025-09-04 16:07:07 +0200

    feat: enhance DICOM processing by adding paths and saving updated DataFrame to a pickle file

--- file_manipulation.py
+++ file_manipulation.py
@@ -1,11 +1,12 @@
 #%%
+from typing import List
 import pandas as pd
-import logging
-import json
-import os
+import pydicom, logging, json, glob, os
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.ERROR)
+
 
 class Counter:
     count_var = 0  # static counter variable
@@ -35,7 +36,6 @@ def get_json_info(folder: os.PathLike) -> dict:
         # print(f"Counter: {Counter.count()}")
     return json_info
 
-# def parse_json_tags(json_info: dict, key: str) -> List[str]:
 
 def make_table(path: str)-> None:
     """
@@ -53,13 +53,14 @@ def make_table(path: str)-> None:
     template_dict = make_header_dict(folders)
     dataframe = fill_table(template_dict, folders)
     os.chdir(original_path)
-    print(f"JSON keys: {template_dict}")
+    # print(f"JSON keys: {template_dict}")
     return dataframe
+
 
 def make_header_dict(folders: list) -> dict:
     json_keys = {'subject': set(), 'lesion': set()}
     logger.info('Creating header dictionary template...')
-    for folder in folders:
+    for folder in tqdm(folders, desc="Collecting JSON keys"):
         logger.info('\n---------------------------------------')
         logger.info(f'current folder path: {folder}')
         json_info = get_json_info(folder)
@@ -76,11 +77,12 @@ def make_header_dict(folders: list) -> dict:
             ['L3_' + key for key in list(json_keys['lesion'])]
     return {key: [] for key in template_keys}
 
+
 def fill_table(template_dict: dict, folders: list) -> pd.DataFrame:
     logger.info('Filling the table with data...')
     all_keys = list(template_dict.keys())
     all_keys.remove('record_id')
-    for folder in folders:
+    for folder in tqdm(folders, desc="Filling table"):
         logger.info('\n---------------------------------------')
         logger.info(f'current folder path: {folder}')
         json_info = get_json_info(folder)
@@ -106,6 +108,7 @@ def fill_table(template_dict: dict, folders: list) -> pd.DataFrame:
     template_dict = additional_columns(template_dict)
     return pd.DataFrame(template_dict)
 
+
 def additional_columns(template_dict: dict) -> dict:
     lesion_cols = ['L1_lesion', 'L2_lesion', 'L3_lesion']
     lesion_counts = []
@@ -120,3 +123,98 @@ def additional_columns(template_dict: dict) -> dict:
                         [('lesion_count', lesion_counts)] +
                         list(template_dict.items())[1:])
     return template_dict
+
+
+def add_dicom_and_annotation_paths(df, dicom_dir):
+
+    views = ["MLO", "CC"]
+    laterality = ["L", "R"]
+
+    for v in views:
+        for lat in laterality:
+            df[f'dicom_path_{v}_{lat}'] = None
+            df[f'annotation_path_{v}_{lat}'] = None
+
+    dicom_files = glob.glob(os.path.join(dicom_dir, "**", "resources", "DICOM", "files", "*.dcm"), recursive=True)
+
+    dicom_info = {}
+    for dcm_path in tqdm(dicom_files, desc="Adding paths"):
+        try:
+            dcm = pydicom.dcmread(dcm_path, stop_before_pixels=True)
+            dicom_name = os.path.splitext(os.path.basename(dcm_path))[0]
+            
+            lat = dcm.ImageLaterality if "ImageLaterality" in dcm else None
+            view = dcm.ViewPosition if "ViewPosition" in dcm else None
+
+            if view == "MLO":
+                pass
+            elif view == "CC":
+                pass
+            # elif view in ["LL", "RL", "LM", "LMO"]:
+            #     if hasattr(dcm, "ViewCodeSequence") and len(dcm.ViewCodeSequence) > 0:
+            #         code_meaning = dcm.ViewCodeSequence[0].get("CodeMeaning", None)
+            #         if code_meaning in ["medio-lateral oblique",
+            #                             "medio-lateral",
+            #                             "latero-medial oblique",
+            #                             "latero-medial"]:
+            #             view = "ML"
+            #         elif code_meaning == "cranio-caudal":
+            #             view = "CC"
+            #         else:
+            #             print(f"Unrecognized CodeMeaning: {code_meaning} in {dcm_path}")
+            # else:
+            #     print(f"Unrecognized ViewPosition: {view} in {dcm_path}")
+            else:
+                continue
+            if lat and view:
+                dicom_info[dicom_name] = (dcm_path, view.upper(), lat.upper())
+        except Exception as e:
+            print(f"Error reading {dcm_path}: {e}")
+
+    seg_files = glob.glob(os.path.join(dicom_dir, "**", "resources", "annotations", "files", "**", "segmentation_*.nii.gz"), recursive=True)
+    seg_info = {os.path.basename(s).replace("segmentation_", "").replace(".nii.gz", ""): s for s in seg_files}
+
+    for idx, row in df.iterrows():
+        record_id = row['record_id']
+        for dicom_name, (dcm_path, view, lat) in dicom_info.items():
+            if record_id in dcm_path:
+                df.at[idx, f'dicom_path_{view}_{lat}'] = dcm_path
+                ann_path = seg_info.get(dicom_name, None)
+                if dicom_name and ann_path:
+                    df.at[idx, f'annotation_path_{view}_{lat}'] = ann_path
+
+    return df
+
+def drop_ambiguous_rows(df):
+    initial_count = len(df)
+    #get patientclass 3 with annotation_path not null
+    df = df[~((df['patientclass'] == 2) & (
+        df[['annotation_path_CC_L', 'annotation_path_CC_R', 'annotation_path_MLO_L', 'annotation_path_MLO_R']].notnull().sum(axis=1) > 1
+    ))]
+    final_count = len(df)
+    print(f"Dropped {initial_count - final_count} ambiguous rows with patientclass 3.")
+    return df
+
+
+def drop_na(df, subset_cols: List[str]):
+    initial_count = len(df)
+    #get patientclass 3 with annotation_path not null
+    df.dropna(subset=subset_cols, how='all', inplace=True)
+    final_count = len(df)
+    print(f"Dropped {initial_count - final_count} rows with all null values in {subset_cols}.")
+    return df
+
+
+def filter_both_views_present_or_absent(df):
+    cc_present = df[['annotation_path_CC_L', 'annotation_path_CC_R']].notnull().any(axis=1)
+    mlo_present = df[['annotation_path_MLO_L', 'annotation_path_MLO_R']].notnull().any(axis=1)
+    # Keep rows where:
+    # 1. Both CC and MLO are present, OR
+    # 2. Both CC and MLO are absent
+    # we can take the dropped cases for pretraining of C() and S()
+    keep_mask = (cc_present & mlo_present) | (~cc_present & ~mlo_present)
+
+    subset_data = df[keep_mask]
+
+    print(f"Number of cases kept (both views present or both absent): {len(subset_data)}")
+    return subset_data
--- main.py
+++ main.py
@@ -3,35 +3,48 @@ import os
 import sys
 sys.path.append('./code')
 import pandas as pd
-from pydicom import dcmread
+from file_manipulation import *
+from rts2nii import rtstruct2nii
+import pickle
 
-os.chdir('/users/project1/pt01190/EUCAIM-PG-GUM/code')
+# def main():
 
-# DICOM_DIR = os.path.join('../euc_gumed_u6_v2')
-DICOM_DIR = os.path.join('../UC6')
-# CSV_FILE = os.path.join('../EuCanImageUseCase68_1.csv')
-CSV_FILE = os.path.join('../EuCanImageUseCase68_v2.csv')
-SUBJECT_DIR = sorted([
-    d for d in os.listdir(DICOM_DIR)
-    if os.path.isdir(os.path.join(DICOM_DIR, d))
-])
+if __name__ == '__main__':
+    os.chdir('/users/project1/pt01190/EUCAIM-PG-GUM/code')
 
-data = pd.read_csv(CSV_FILE,
-                   delimiter=',',
-                   encoding='utf-8')
+    # DICOM_DIR = os.path.join('../euc_gumed_u6_v2')
+    DICOM_DIR = os.path.join('../UC6')
+    CSV_FILE = os.path.join('../EuCanImageUseCase68_v2.csv')
+    SUBJECT_DIR = sorted([
+        d for d in os.listdir(DICOM_DIR)
+        if os.path.isdir(os.path.join(DICOM_DIR, d))
+    ])
 
-subset_data = data[data['patientclass'] == 0]
-# %%
-from file_manipulation import make_table
-if __name__ == "__main__":
+    data = pd.read_csv(CSV_FILE,
+                    delimiter=',',
+                    encoding='utf-8')
+
+    subset_data = data[data['patientclass'] == 0]
+
+    print("Creating table from DICOM directories...")
     df = make_table(DICOM_DIR)
 
-# %%
-merged_data = pd.merge(data, df, how='inner', on='record_id')
-# %%
-from rts2nii import rtstruct2nii
+    merged_data = pd.merge(data, df, how='inner', on='record_id')
 
-if __name__ == "__main__":
-    rtstruct2nii(DICOM_DIR)
+    # rtstruct2nii(DICOM_DIR)
+    # print('RTSTRUCT to NIfTI conversion completed.')
+
+    updated_df = add_dicom_and_annotation_paths(merged_data, DICOM_DIR)
+    updated_df = drop_na(updated_df, ["patientclass"])
+    updated_df = drop_ambiguous_rows(updated_df)
+    updated_df = filter_both_views_present_or_absent(updated_df)
+
+    updated_df.reset_index(drop=True, inplace=True)
+
+    fname = 'dataset.pkl'
+    with open(fname, 'wb') as f:
+        pickle.dump(updated_df, f)
+        print(f'File saved as {fname}.')
+
 
 # %%
