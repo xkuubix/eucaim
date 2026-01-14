1a930388fddb6eceb046f447bc6c5b1700917c9f
xkuubix <buler.jakub@gmail.com>
2026-01-14 11:15:51 +0100

    fix: migrate from neptune to wandb and update activation handling in PatchUNet

--- show_unet_reconstruction.py
+++ show_unet_reconstruction.py
@@ -7,7 +7,7 @@ from models import PatchUNet
 import torch
 import utils
 import yaml
-import neptune
+import wandb
 import matplotlib.pyplot as plt
 import torch
 from net_utils import _dice_from_logits_map
@@ -25,6 +25,7 @@ device = torch.device(selected_device if torch.cuda.is_available() else "cpu")
 run = None
 
 dataloaders = utils.get_fold_dataloaders(config, 0)
+activation = config.get('activation', 'prelu').lower()
 
 unet = PatchUNet(
     config,
@@ -34,7 +35,7 @@ unet = PatchUNet(
     channels=(16, 32, 64, 128, 256, 512),
     strides=(2, 2, 2, 2, 2),
     num_res_units=2,
-    act='PReLU',
+    act=activation,
     dropout=0.1,
     kernel_size=3,
     up_kernel_size=3,
@@ -42,39 +43,47 @@ unet = PatchUNet(
     bias=False, # using norm
 ).to(device)
 
-model_path = None
-if model_path is not None:
+
+model_path = "/users/scratch1/jbuler/eucaim/models/MAM-1036_best.pth" # 256
+# model_path = "/users/scratch1/jbuler/eucaim/models/MAM-1120_best.pth" # 512
+if model_path:
     unet.load_state_dict(torch.load(model_path, map_location=device))
 
 loss_fn_name = config['training_plan'].get('loss_function', 'dice')
 criterion = utils.get_loss_function(loss_fn_name=loss_fn_name, device=device)
 
 unet.eval()
+i = 0
 with torch.no_grad():
-    for batch in dataloaders['val']:
+    for batch in dataloaders['test']:
         images = batch['image'].to(device)
         masks = batch['annotation'].to(device)
-
         preds_patched, masks_patched, instances_ids = unet(images, masks)
-        print(preds_patched.shape, masks_patched.shape)
         pred, patch_count = unet.patcher.reconstruct_image_from_patches(preds_patched, instances_ids, image_shape=images.shape)  # (c, h, w)
         mask, _ = unet.patcher.reconstruct_image_from_patches(masks_patched, instances_ids, image_shape=images.shape) if masks is not None else (None, None)
-        print(pred.shape, mask.shape)
-        loss = criterion(preds_patched, masks_patched)
-        print(_dice_from_logits_map(pred, mask, 0.9, patch_count))
-        print(float(loss.item()))
+
+        fig, axs = plt.subplots(1, 4, figsize=(12, 6))
+        axs[0].imshow(images[0].cpu(), cmap='gray')
+        axs[0].set_title('input')
+        axs[1].imshow(mask[0].cpu(), cmap='gray')
+        axs[1].set_title('gt')
         probs = torch.sigmoid(pred)
         valid_mask = patch_count > 0
         probs[~valid_mask] = 0.
         preds = (probs > 0.5).float()
-        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-        axs[0].imshow(images[0].cpu(), cmap='gray')
-        axs[0].set_title('Input Image')
-        axs[1].imshow(mask[0].cpu(), cmap='gray')
-        axs[1].set_title('Ground Truth Mask')
         axs[2].imshow(preds[0].cpu(), cmap='gray')
-        axs[2].set_title('Predicted Mask')
-        plt.show()
-        break
+        axs[2].set_title('pred 0.5 th')
+        axs[3].imshow(probs[0].cpu(), cmap='hot')
+        axs[3].set_title('pred raw')
+        
+        for ax in axs:
+            ax.set_xticks([])
+            ax.set_yticks([])
 
+        fig.suptitle(f'Patch Size: {config["data"]["patch_size"]} (n={preds_patched.shape[0]}), Overlap: {config["data"]["overlap"]} | Image: {images.shape[-2]}x{images.shape[-1]}px', fontsize=16)
+        plt.tight_layout()
+        plt.show()
+        i += 1
+        if i == 10:
+            break
 # %%
